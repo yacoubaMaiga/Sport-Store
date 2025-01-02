88ecf3c1207cddcde61ad96834ec3974715fe5a4
yacoubaMaiga <maigayacoubaa7@gmail.com>
2025-01-02 20:02:09 +0000

    Add of product and category tables

---
+++ database/migrations/2025_01_02_195419_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('idProduct');
+            $table->string('productName');
+            $table->text('productDescription');
+            $table->double('productPrice');
+            $table->integer('productStock');
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
